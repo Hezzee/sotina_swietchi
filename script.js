c3e92e8c63dddff5333a34a4112f67ed4b44ba1e
Heze <vinogradovsa2005@gmail.com>
2025-04-22 23:33:09 +0200

    adaptation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('.header_burger').click(function() {
-        $('.close_header_burger, .bg_menu, .header_burger').toggleClass('active');
+        $('.close_header_burger, .bg_menu, .header_burger, .header_h1').toggleClass('active');
         $('body,html').toggleClass('poshelnahuiscroll');
     })
     $('.close_header_burger').click(function() {
-        $('close_header_burger, .bg_menu, .header_burger').removeClass('active');
+        $('close_header_burger, .bg_menu, .header_burger, .header_h1').removeClass('active');
         $('body,html').removeClass('poshelnahuiscroll');
     });
 
